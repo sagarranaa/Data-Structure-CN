d8697f54a0df0b3853e71a68463b1adfde3096f8
Rana  Sagar <64120304+sagar-demo@users.noreply.github.com>
2022-06-25 22:16:54 +0530

    Add files via upload

---
+++ removeduplicate.java
@@ -0,0 +1,24 @@
+package sagar.company;
+import java.util.ArrayList;
+public class removeduplicate {
+    public static ArrayList<Integer>removedublicate(int arr[]){
+        ArrayList <Integer>result=new ArrayList<>();
+        result.add(arr[0]);
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]!=arr[i-1]){
+                result.add(arr[i]);
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int arr[]={10,10,20,20,30,10};
+        ArrayList<Integer>result=removedublicate(arr);
+        for (int i=0;i<result.size();i++){
+            System.out.println(result.get(i));
+        }
+
+    }
+
+}
